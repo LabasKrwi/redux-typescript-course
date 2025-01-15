37722e2c67c71a1a58ec10b6a218b4fcf947d09b
Илья Коперман <ilukhankoperman@gmail.com>
2025-01-15 10:08:50 +0300

    1.0.0

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,26 @@
+import { useAppDispatch } from '../hooks';
+import {toggleComplete, removeTodo} from '../store/todoSlice';
+
+interface ITodoItemProps {
+    id: string,
+    title: string,
+    completed: boolean
+}
+
+const TodoItem: React.FC<ITodoItemProps> = ({ id, title, completed }) => {
+  const dispatch = useAppDispatch();
+
+  return (
+    <li>
+      <input
+        type='checkbox'
+        checked={completed}
+        onChange={() => dispatch(toggleComplete(id))}
+      />
+      <span>{title}</span>
+      <span onClick={() => dispatch(removeTodo(id))}>&times;</span>
+    </li>
+  );
+};
+
+export default TodoItem;
