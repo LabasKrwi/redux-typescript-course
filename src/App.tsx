e30aafc7090a675079ca5daff6123c639e9cf702
Илья Коперман <ilukhankoperman@gmail.com>
2025-01-15 11:16:04 +0300

    async

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import {useState} from 'react';
-import { useAppDispatch } from './hooks';
-import { addTodo } from './store/todoSlice';
+import {useState, useEffect} from 'react';
+import { useAppDispatch, useAppSelector } from './hooks';
+import { fetchTodos, addNewTodo } from './store/todoSlice';
 import NewTodoForm from './components/NewTodoForm';
 import TodoList from './components/TodoList';
 import './App.css';
@@ -9,13 +9,19 @@ import './App.css';
 function App() {
   const [text, setText] = useState('');
   const dispatch = useAppDispatch();
+  const {loading, error} = useAppSelector(state => state.todos);
+
 
   const handleAction = () => {
     if(text.trim().length) {
-      dispatch(addTodo(text));
+      dispatch(addNewTodo(text));
       setText('');
     }
   }
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch])
 
   return (
     
@@ -25,6 +31,10 @@ function App() {
           updateText={setText}
           handleAction={handleAction}
         />
+
+          {loading  && <h2>Loading...</h2>}
+          {error &&  <h2>An error occured: {error}</h2>}
+
         <TodoList />
       </div>
     
